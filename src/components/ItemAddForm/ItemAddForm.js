c64f9b25b22e134381b2af3f154d039c929bff3e
Sasha Prokofyev <ablehope@yandex.by>
2019-10-08 13:52:49 +0300

    my first react app

---
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './ItemAddForm.css';
+
+export default class ItemAddForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            label: ''
+        }
+    }
+
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ''
+        });
+    }
+
+    render() {
+        return (
+            <form className='item-add-form d-flex' onSubmit = {this.onSubmit}>
+                <input type='text' className='form-control'
+                 onChange = {this.onLabelChange} placeholder = 'What needs to be done'
+                 value= {this.state.label} />
+                <button className='btn btn-outline-success'>
+                Add
+                </button>
+            </form>
+        )
+    };
+};
